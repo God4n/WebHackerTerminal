a9fa5449d88b02f065cca82f7b9cee7ea932119e
Tadeo Goday <tadeo.goday@gmail.com>
2023-08-23 00:33:11 -0300

    Add: login panel

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var pass = 'testpasswd';
+
+$(document).ready(function() {
+    let passwordForm = document.getElementById("passwordForm");
+    passwordForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        verifyPassword(document.getElementById("passwd").value);
+    });
+});
+
+function verifyPassword(password)
+{
+    if (password == pass) {
+        showGranted();
+    }
+    else {
+        showFailed();
+    }
+}
+
+
+// Login Failed
+
+function showFailed()
+{
+    document.getElementById("password-box").style.display = "none";
+    document.getElementById("denied-box").style.display = "block";
+    document.getElementById("passwd").value = '';
+    setTimeout(revertToPassword, 1500);
+}
+
+function revertToPassword()
+{
+    document.getElementById("denied-box").style.display = "none";
+    document.getElementById("password-box").style.display = "block";
+}
+
+
+// Correct Login
+
+function showGranted()
+{
+    document.getElementById("password-box").style.display = "none";
+    document.getElementById("granted-box").style.display = "block";
+    setTimeout(showAccessing, 1500);
+}
+
+function showAccessing()
+{
+    document.getElementById("granted-box").style.display = "none";
+    document.getElementById("accessing-box").style.display = "block";
+    startProgressBar();
+}
+
+function startProgressBar() 
+{
+    var a = 0;
+    if (a == 0) 
+    {
+        a++;
+        var width = 1;
+        var pg = document.getElementById("progressBar");
+        var interval = setInterval(increasePercentage, 30);
+
+        function increasePercentage() 
+        {
+            if (width >= 100) 
+            {
+                clearInterval(interval);
+                setTimeout(showLoading, 500);
+            } 
+            else 
+            {
+                width++;
+                pg.style.width = width + "%";
+                $("#accessing-loading-percentage").html(width  + "%");
+            }
+        }
+    }
+}
+
+function showLoading()
+{
+    document.getElementById("password-box").style.display = "none";
+    document.getElementById("accessing-box").style.display = "none";
+    setTimeout(loadSite, 1500);
+}
+
+function loadSite()
+{
+    window.location.href = "/home.html"
+}
+
