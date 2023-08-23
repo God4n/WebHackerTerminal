4c41c95fb2a30367abd73539ab4a99998bb26156
Tadeo Goday <tadeo.goday@gmail.com>
2023-08-23 16:54:15 -0300

    Testing Cookies

--- js/main.js
+++ js/main.js
@@ -1,93 +1,32 @@
-var pass = 'testpasswd';
-
-$(document).ready(function() {
-    let passwordForm = document.getElementById("passwordForm");
-    passwordForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        verifyPassword(document.getElementById("passwd").value);
-    });
-});
-
-function verifyPassword(password)
-{
-    if (password == pass) {
-        showGranted();
-    }
-    else {
-        showFailed();
-    }
-}
-
-
-// Login Failed
-
-function showFailed()
-{
-    document.getElementById("password-box").style.display = "none";
-    document.getElementById("denied-box").style.display = "block";
-    document.getElementById("passwd").value = '';
-    setTimeout(revertToPassword, 1500);
-}
-
-function revertToPassword()
-{
-    document.getElementById("denied-box").style.display = "none";
-    document.getElementById("password-box").style.display = "block";
-}
-
-
-// Correct Login
-
-function showGranted()
-{
-    document.getElementById("password-box").style.display = "none";
-    document.getElementById("granted-box").style.display = "block";
-    setTimeout(showAccessing, 1500);
-}
-
-function showAccessing()
-{
-    document.getElementById("granted-box").style.display = "none";
-    document.getElementById("accessing-box").style.display = "block";
-    startProgressBar();
-}
-
-function startProgressBar() 
-{
-    var a = 0;
-    if (a == 0) 
-    {
-        a++;
-        var width = 1;
-        var pg = document.getElementById("progressBar");
-        var interval = setInterval(increasePercentage, 30);
-
-        function increasePercentage() 
-        {
-            if (width >= 100) 
-            {
-                clearInterval(interval);
-                setTimeout(showLoading, 500);
-            } 
-            else 
-            {
-                width++;
-                pg.style.width = width + "%";
-                $("#accessing-loading-percentage").html(width  + "%");
-            }
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
         }
     }
+    return null;
 }
 
-function showLoading()
-{
-    document.getElementById("password-box").style.display = "none";
-    document.getElementById("accessing-box").style.display = "none";
-    setTimeout(loadSite, 1500);
-}
+function isLogged() {
+    const accessCookieValue = getCookie('access');
+    
+    if (accessCookieValue !== null) {
+        if (accessCookieValue === 'true') {
+            // Hacer algo si el valor de la cookie es true
+            console.log('El valor de la cookie "access" es true. Acceso permitido.');
+            // Aquí puedes realizar acciones específicas cuando el acceso es permitido.
+        } else if (accessCookieValue === 'false') {
+            // Hacer algo si el valor de la cookie es false
+            console.log('El valor de la cookie "access" es false. Acceso denegado.');
+            // Aquí puedes realizar acciones específicas cuando el acceso es denegado.
+        } else {
+            console.log('El valor de la cookie "access" no es válido.');
+        }
+    } else {
+        console.log('La cookie de sesión "access" no se encontró.');
+    }
 
-function loadSite()
-{
-    window.location.href = "/home.html"
+    console.log(accessCookieValue);
 }
-
